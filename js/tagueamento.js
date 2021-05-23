cc2d8d445394210938c665bf486deabd47e1dca5
DieRamos <50583017+DieRamos@users.noreply.github.com>
2021-05-23 14:37:16 -0300

    Add files via upload

---
+++ js/tagueamento.js
@@ -0,0 +1,31 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-12345-6', 'auto');
+ 
+
+ga('send', {
+  hitType: 'pageview',
+  page: Location.pathname
+});
+
+var entreEmContato = document.getElementById("contatoId");
+entreEmContato.addEventListener("click", function(event) {
+    ga('entreEmContato.send', {
+        hitType: 'event',
+        eventCategory: 'menu',
+        eventAction: 'entre_em_contato',
+        eventLabel:  'link_externo'
+    });
+});
+
+var downloadPdf = document.getElementById("downloadPdfId");
+downloadPdf.addEventListener("click", function(event) {
+    ga('downloadPdf.send', {
+        hitType: 'event',
+        eventCategory: 'menu',
+        eventAction: 'download_pdf',
+        eventLabel:  'download_pdf'
+    });
+});
